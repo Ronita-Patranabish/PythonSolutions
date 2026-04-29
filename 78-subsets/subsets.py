e1098c418df8e2983b693509f4c62b68eb9dc500
Ronita-Patranabish <ronita.patranabish.cs28@iilm.edu>
2026-04-29 12:13:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (59.52%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums):
+        result = []
+        
+        def backtrack(index, path):
+            # add current subset
+            result.append(path[:])
+            
+            for i in range(index, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+        
+        backtrack(0, [])
+        return result
