9830cdda35ffa21d03cdb489aa897d33fe554158
Ronita-Patranabish <ronita.patranabish.cs28@iilm.edu>
2026-04-29 10:56:24 +0530

    Time: 15 ms (20.06%) | Memory: 16 MB (73.58%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals):
+        # Step 1: Sort intervals based on start time
+        intervals.sort()
+        
+        merged = []
+        
+        for interval in intervals:
+            # If merged list is empty OR no overlap
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                # Overlapping → merge
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        
+        return merged
